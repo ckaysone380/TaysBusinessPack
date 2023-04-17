76b15142873c53cf0724a43ea83d7a0047077fd6
TayMcKenzieSA <taylorocarroll@gmail.com>
2023-04-17 20:28:38 +1200

    Added Files Via GitHub Desktop App

---
+++ FILES/FIVEM/taysbusinesspack/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Generated with DurtyClothTool --
+-- Created by TayMcKenzieNZ --
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_taysbusinesspack.meta',
+  'mp_f_freemode_01_mp_f_taysbusinesspack.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_taysbusinesspack.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_taysbusinesspack.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
